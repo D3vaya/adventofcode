12d80154ee9ac470e826e31cabc22af713c381a4
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-10 11:20:49 -0300

    feat: Day 3 ⭐️

--- AOC-2022/Day3_Rucksack_Reorganization/index.ts
+++ AOC-2022/Day3_Rucksack_Reorganization/index.ts
@@ -9,4 +9,61 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw`;
 
-export const rucksackReorganization = () => {};
+const LETTERS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const lettersUpperCase = LETTERS.map((l) => l.toLocaleUpperCase());
+const dictionary = LETTERS.concat(lettersUpperCase);
+
+const mapDictionary = new Map(dictionary.map((letter, i) => [letter, i+1]));
+
+const evaluateMatches = (backpack: string): string => {
+  const compartmentA = backpack.slice(0, backpack.length / 2);
+  const compartmentB = backpack.slice(backpack.length / 2, backpack.length);
+  let letterMatch = "";
+
+  compartmentA.split("").forEach((letter) => {
+    if (compartmentB.includes(letter)) letterMatch = letter;
+  });
+
+  return letterMatch;
+};
+
+export const rucksackReorganization = () => {
+  const arrayBackpack = INPUT.split("\n");
+  let sumPriority = 0;
+  arrayBackpack.forEach((backpack) => {
+    const letterMatch = evaluateMatches(backpack);
+    sumPriority += mapDictionary.get(letterMatch)!;
+  });
+  return sumPriority;
+};
+
+// Success Result 7908 ⭐️
+rucksackReorganization();
