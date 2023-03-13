06e10f13b5533c87d5921161473ccc77e3a6bb2b
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-13 20:58:44 -0300

    feat: day 10

---
+++ AOC-2022/Day10_Cathode-Ray_Tube/index.ts
@@ -0,0 +1,1 @@
+export const cathodeRayTube = () =>{}
--- AOC-2022/index.ts
+++ AOC-2022/index.ts
@@ -5,3 +5,6 @@ export * from "./Day4_Camp_Cleanup";
 export * from "./Day5_Supply_Stacks";
 export * from "./Day6_Tuning_Trouble";
 export * from "./Day7_No_Space_Left_On_Device";
+export * from "./Day8_Treetop_Tree_House";
+export * from "./Day9_Rope_Bridge";
+export * from "./Day10_Cathode-Ray_Tube";
