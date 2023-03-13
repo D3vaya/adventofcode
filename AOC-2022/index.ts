97ea7b58f1a35cdba509491a1b4344086d56ea01
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-13 16:53:51 -0300

    feat: day 7

--- AOC-2022/index.ts
+++ AOC-2022/index.ts
@@ -3,3 +3,5 @@ export * from "./Day2_Rock_Paper_Scissors";
 export * from "./Day3_Rucksack_Reorganization";
 export * from "./Day4_Camp_Cleanup";
 export * from "./Day5_Supply_Stacks";
+export * from "./Day6_Tuning_Trouble";
+export * from "./Day7_No_Space_Left_On_Device";
