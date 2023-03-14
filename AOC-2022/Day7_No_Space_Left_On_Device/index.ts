2e4b4dff3385c2f2e21bbf79580423a3a0b440a7
Jc Ayala <j.ayala.higuera@gmail.com>
2023-03-14 16:00:26 -0300

    feat: develop day 7

--- AOC-2022/Day7_No_Space_Left_On_Device/index.ts
+++ AOC-2022/Day7_No_Space_Left_On_Device/index.ts
@@ -3,7 +3,6 @@ import { readFileSync } from "fs";
 import { Tree } from "./trees";
 
 const INPUT = readFileSync(join(__dirname, "input.txt"), "utf8").split("\n");
-
 const INPUT_MOCK = `$ cd /
 $ ls
 dir a
@@ -28,18 +27,23 @@ $ ls
 5626152 d.ext
 7214296 k`.split("\n");
 
-const COMMANDS = {
-  "$ cd": "",
-  dir: "",
-  ls: "",
-};
-const CMD_CD = "$";
-const CMD_DIR = "dir";
+interface Folder {
+  name: string;
+  folders: Folder[];
+  folderSize: number;
+  files: FileFolder[];
+  folderParent: Folder | null;
+}
+interface FileFolder {
+  fileName: string;
+  fileWeight: number;
+}
 
 interface CMD {
   command: string;
   terminal: Terminal;
-  execute: () => boolean;
+
+  execute: () => void;
 }
 
 // COMMANDS
@@ -52,8 +56,17 @@ class DirCommand implements CMD {
     this.terminal = t;
   }
 
-  execute(): boolean {
-    return false;
+  execute(): void {
+    const [cmd, nameFolder] = this.command.split(" ");
+    const newFolder: Folder = {
+      name: nameFolder,
+      files: [],
+      folders: [],
+      folderSize: 0,
+      folderParent: this.terminal.sessionTerminal,
+    };
+    this.terminal.sessionTerminal.folders.push(newFolder);
+    this.terminal.tree.folders.push(newFolder);
   }
 }
 
@@ -61,14 +74,35 @@ class CreateFileCommand implements CMD {
   command: string;
   terminal: Terminal;
   constructor(cmd: string, t: Terminal) {
+    //console.log('[LOG] 🚧 ', {cmd})
     this.command = cmd;
     this.terminal = t;
   }
 
-  execute(): boolean {
-    return false;
+  execute(): void {
+    const [fileWeight, fileName] = this.command.split(" ");
+    const newFile: FileFolder = {
+      fileName,
+      fileWeight: Number(fileWeight),
+    };
+    this.terminal.sessionTerminal.folderSize += newFile.fileWeight;
+    this.summation(this.terminal.sessionTerminal, newFile.fileWeight);
+    // if (this.terminal.sessionTerminal.folderParent) {
+    //   this.terminal.sessionTerminal.folderParent.folderSize +=
+    //     newFile.fileWeight;
+    // }
+    this.terminal.sessionTerminal.files.push(newFile);
+    this.terminal.tree.files.push(newFile);
+  }
+
+  summation(folder: Folder, fileWeight: number) {
+    if (folder.folderParent) {
+      folder.folderParent.folderSize += fileWeight;
+      this.summation(folder.folderParent, fileWeight);
+    }
   }
 }
+
 class LsCommand implements CMD {
   command: string;
   terminal: Terminal;
@@ -77,44 +111,214 @@ class LsCommand implements CMD {
     this.terminal = t;
   }
 
-  execute(): boolean {
-    return false;
+  execute(): void {
+    //this.terminal.printSession();
   }
 }
+
 class CdCommand implements CMD {
   command: string;
   terminal: Terminal;
+
   constructor(cmd: string, t: Terminal) {
     this.command = cmd;
     this.terminal = t;
   }
 
-  execute(): boolean {
-    console.log('- / (Dir)')
-    this.terminal.tree.insert(1)
-    console.log("[LOG] 🚧 TREE", this.terminal.tree);
+  private isReturn(cmd: string): boolean {
+    if (cmd === "..") return true;
     return false;
+  }
+
+  execute(): void {
+    const [symbol, cmd, path] = this.command.split(" ");
+
+    const isBack = this.isReturn(path);
+    let directory: Folder;
+    if (isBack) {
+      directory = this.terminal.sessionTerminal.folderParent!;
+    } else {
+      this.terminal.backupDir = this.terminal.sessionTerminal;
+      directory = this.terminal.sessionTerminal.folders.find(
+        (f) => f.name === path
+      )!;
+    }
+
+    this.terminal.sessionTerminal = directory;
   }
 }
 
 class Terminal {
-  tree: Tree;
-  arrayCommands: CMD[] = [];
-  commands: Map<string, CMD> = new Map();
+  // tree: Tree;
+  tree: Folder;
+  backupDir: Folder;
+  sessionTerminal: Folder;
+  rootDirTmp: Folder;
+  private mapCommands: Map<string, CMD> = new Map();
+  private arrayCommands: string[] = [];
 
   constructor(cmds: string[]) {
     if (!cmds) throw `${cmds} undefined`;
     if (cmds.length === 0) throw `${cmds} empty`;
-    this.tree = new Tree();
-    cmds.forEach((command) => {});
+    this.arrayCommands = cmds;
+
+    this.sessionTerminal = {
+      name: "root",
+      folders: [
+        {
+          name: "/",
+          files: [],
+          folders: [],
+          folderSize: 0,
+          folderParent: null,
+        },
+      ],
+      folderSize: 0,
+      files: [],
+      folderParent: null,
+    };
+    //this.tree = this.sessionTerminal;
+    this.tree = {
+      name: "/",
+      files: [],
+      folders: [],
+      folderSize: 0,
+      folderParent: null,
+    };
+    this.rootDirTmp = this.tree;
+    this.backupDir = this.sessionTerminal;
   }
 
-  exe() {
-    const c = new CdCommand("$ cd /", this);
-    c.execute();
+  runCommands(): void {
+    this.arrayCommands.forEach((command) => {
+      const cmdArray = command.split(" ");
+      if (cmdArray.length === 3) {
+        const Command = new CdCommand(command, this);
+        Command?.execute();
+        return;
+      } else {
+        if (cmdArray.length === 2) {
+          const [cmd, name] = cmdArray;
+          if (cmd === "dir") {
+            const Command = new DirCommand(command, this);
+            Command?.execute();
+          }
+          if (name === "ls") {
+            const Command = new LsCommand(command, this);
+            Command?.execute();
+          }
+          if (cmd !== "dir" && name !== "ls") {
+            const Command = new CreateFileCommand(command, this);
+            Command?.execute();
+          }
+        }
+      }
+    });
+  }
+
+  calculateDirectorySize(counter: number, folder: Folder) {
+    if (folder.folders.length > 0) {
+      folder.folders.forEach((f) => {
+        counter += f.folderSize;
+        return this.calculateDirectorySize(counter, f);
+      });
+    } else {
+      counter += folder.folderSize;
+    }
+    return counter;
+  }
+
+  printSession() {
+    console.table(this.tree);
+  }
+
+  findIdealSize() {
+    let tmp = 0;
+    this.tree.folders.forEach((f) => {
+      tmp += f.folderSize;
+    });
+    // const r = this.tree.folders
+    //   .filter((f) => f.folderSize <= 100000)
+    //   .sort((a, b) => a.folderSize + b.folderSize);
+    // return r[0].folderSize;
+  }
+
+  sum(tree: Folder, count: number, counter: number, tmp: number) {
+    const arr: number[] = [];
+    tree.folders.forEach(f => {
+      let t = 0;
+      //console.log('[LOG] 🚧 ', {f})
+      const len = tree.folders.length;
+
+      for (let i = 0; i <= len; i++) {
+        if (typeof f.folders[i]?.folderSize !== "undefined") {
+          t += (f.folderSize + f.folders[i]?.folderSize);
+          console.log('[LOG] 🚧 ', {t})
+          if (t <= 100000) {
+            tmp = t;
+            arr.push(tmp);
+          }
+          // } else {
+          //   t = 0;
+          //   arr.push(tmp);
+          // }
+        }
+      }
+    })
+    //if (tree.folders !== null) {
+      
+      // tree.folders.forEach((f, i) => {
+      //   counter++;
+      //   count += f.folderSize;
+      //   tmp = count;
+      //   if (counter % 2 === 0) {
+      //     if (count <= 100000) {
+      //       tmp = count;
+      //     } else {
+
+      //       return tmp;
+      //     }
+      //   }
+
+      //   this.sum(f, count, counter, tmp);
+      // });
+    //}
+
+    return arr;
   }
 }
 
+const M = `$ cd /
+$ ls
 
-const t = new Terminal(['$ cd /'])
-t.exe()
+dir b
+$ cd b
+9800 script.ts
+10500 example.html
+$ cd ..
+14848514 b.txt
+dir a
+$ cd a
+$ ls
+12345 type.ts
+22345 type.js
+dir __test__
+$ ls
+$ cd __test__
+48514 hola-mundo.txt
+$ ls
+$ cd ..
+$ ls
+$ cd ..
+`.split("\n");
+
+const terminal = new Terminal(INPUT);
+terminal.runCommands();
+console.log("[LOG] 🚧 sum", terminal.sum(terminal.tree, 0, 0, 0));
+console.log("[LOG] 🚧 ", terminal.findIdealSize());
+
+// console.log(
+//   "COUNTER",
+//   terminal.calculateDirectorySize(0, terminal.sessionTerminal)
+// );
+//terminal.printSession();
